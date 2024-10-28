1a20a182bf7041f0b8df1a4d1bee5644fd48f155
yashwanth-reddy909 <yaswanthreddy909@gmail.com>
2024-10-28 14:59:47 +0530

    feat: added an new set-up nudging

---
+++ ozone-apx-nudge-staging/main.tf
@@ -0,0 +1,89 @@
+/* Global variables */
+
+locals {
+  production_availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
+}
+
+/* Import Networking Module */
+
+module "networking" {
+  source               = "../tf-modules/networking"
+  region               = var.region
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  private_subnets_cidr = var.private_subnets_cidr
+  availability_zones   = local.production_availability_zones
+}
+
+/* Import default-iam Module */
+
+module "default-iam" {
+  source      = "../tf-modules/default-iam"
+  environment = var.environment
+  region      = var.region
+}
+
+/* Create the EKS Cluster */
+
+resource "aws_eks_cluster" "eks_cluster" {
+  depends_on                = [module.default-iam]
+  name                      = var.cluster_name
+  role_arn                  = module.default-iam.cluster_role_arn
+  version                   = var.cluster_version
+  enabled_cluster_log_types = var.enabled_cluster_log_types
+  tags = {
+    "env" = var.environment
+  }
+  vpc_config {
+    subnet_ids         = concat(module.networking.private_subnets_ids, module.networking.public_subnets_ids)
+    security_group_ids = module.networking.security_groups_ids
+  }
+}
+
+/* Import oidc Module */
+
+module "oidc" {
+  depends_on   = [aws_eks_cluster.eks_cluster]
+  source       = "../tf-modules/oidc"
+  cluster_name = var.cluster_name
+}
+
+/* Import EBS add on */
+
+module "ebs-driver-addon" {
+  depends_on                   = [aws_eks_cluster.eks_cluster, module.oidc, module.ng-multi]
+  source                       = "../tf-modules/ebs-csi-driver"
+  region                       = var.region
+  cluster_name                 = var.cluster_name
+  oidc_id                      = module.oidc.oidc_id
+  aws_iam_oidc_provider_arn    = module.oidc.aws_iam_oidc_provider_arn
+  ebs_snapshotter_force_enable = var.ebs_snapshotter_force_enable
+  ebs_driver_version           = var.ebs_driver_version
+}
+
+/* Import cluster-autoscaler Module */
+
+module "autoscaler" {
+  depends_on                = [aws_eks_cluster.eks_cluster, module.oidc, module.ng-multi]
+  source                    = "../tf-modules/cluster-autoscaler"
+  region                    = var.region
+  aws_account_id            = var.aws_account_id
+  oidc_id                   = module.oidc.oidc_id
+  aws_iam_oidc_provider_arn = module.oidc.aws_iam_oidc_provider_arn
+  cluster_name              = var.cluster_name
+}
+
+/* Import alb-controller Module */
+
+module "alb-controller" {
+  depends_on                = [module.oidc]
+  source                    = "../tf-modules/alb-controller"
+  region                    = var.region
+  aws_account_id            = var.aws_account_id
+  cluster_name              = var.cluster_name
+  oidc_id                   = module.oidc.oidc_id
+  aws_iam_oidc_provider_arn = module.oidc.aws_iam_oidc_provider_arn
+  vpc_id                    = module.networking.vpc_id
+  enabled                   = true
+}
---
+++ ozone-apx-nudge-staging/variables.auto.tfvars
@@ -0,0 +1,20 @@
+/* Variables for AWS environemnt */
+
+region         = "ap-south-1"
+environment    = ""
+aws_account_id = ""
+sso_profile    = "default"
+
+/* Variables for Networking module */
+
+vpc_cidr             = "10.0.0.0/16"
+private_subnets_cidr = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/19"]
+public_subnets_cidr  = ["10.0.96.0/19", "10.0.128.0/19", "10.0.160.0/19"]
+
+/* Variables for EKS */
+
+cluster_name    = ""
+instance_key    = ""
+cluster_version = "1.30"
+# check for the compatible version here: https://docs.aws.amazon.com/eks/latest/userguide/addon-compat.html
+ebs_driver_version = "v1.34.0-eksbuild.1"
