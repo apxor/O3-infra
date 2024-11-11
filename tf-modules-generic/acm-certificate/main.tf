be574e63120e75314f2ff3d9daf58acc4b305e3d
PrudhviApx7 <prudhvi.akula@apxor.com>
2024-11-11 15:11:01 +0530

    feat: removed dns validation

--- tf-modules-generic/acm-certificate/main.tf
+++ tf-modules-generic/acm-certificate/main.tf
@@ -10,39 +10,3 @@ resource "aws_acm_certificate" "cert_acm" {
     create_before_destroy = true
   }
 }
-
-/* dns validation */
-
-data "aws_route53_zone" "selected_zone" {
-  name         = var.root_domain
-  private_zone = false
-}
-
-resource "aws_route53_record" "cert_validation_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert_acm.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.selected_zone.zone_id
-}
-
-/* Handle Certificate Validation */
-
-resource "aws_acm_certificate_validation" "cert_validation" {
-  timeouts {
-    create = "5m"
-  }
-  certificate_arn         = aws_acm_certificate.cert_acm.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
-}
-
-
