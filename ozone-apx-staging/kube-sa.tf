44758d58e7ce3ab65f0423a540f52cabc8b51e1f
yashwanth-reddy909 <yaswanthreddy909@gmail.com>
2025-01-07 15:52:30 +0530

    feat: added resources required for widget downloades

--- ozone-apx-staging/kube-sa.tf
+++ ozone-apx-staging/kube-sa.tf
@@ -10,3 +10,14 @@ module "secor-sa" {
   aws_iam_oidc_provider_arn = module.oidc.aws_iam_oidc_provider_arn
   access_to_s3_buckets      = [module.apx-o3-secor-events.bucket_name]
 }
+
+module "apxorapi-sa" {
+  source                    = "../tf-modules/iam-sa"
+  service_account_name      = "apxorapi-sa"
+  namespace                 = "apxorapi"
+  region                    = var.region
+  oidc_id                   = module.oidc.oidc_id
+  aws_account_id            = var.aws_account_id
+  aws_iam_oidc_provider_arn = module.oidc.aws_iam_oidc_provider_arn
+  access_to_s3_buckets      = [module.apx-o3-dashboard-downloadables.bucket_name]
+}
